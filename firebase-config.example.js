c9a1e70d724202d6840f6579a292c7e5fd963871
Armindo Mambemba <armindoarthur24@gmail.com>
2026-08-20 20:32:09 +0100

    Delete firebase-config.example.js

--- firebase-config.example.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "COLOQUE_AQUI",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.firebasestorage.app",
-  messagingSenderId: "COLOQUE_AQUI",
-  appId: "COLOQUE_AQUI"
-};
